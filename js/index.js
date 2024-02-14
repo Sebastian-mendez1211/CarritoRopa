39a540686753e1f8f8ae9353ea92df5175d1d2c2
sebastian mendez <sebasmendezhernandez2010@gmail.com>
2024-02-14 18:01:38 -0500

    segundo avance del proyecto

--- js/index.js
+++ js/index.js
@@ -1,17 +1,88 @@
-    
-    let total = 0;
-    function agregarProducto(precio) {
-      total += precio;
-      actualizarTotal();
-    }
+const PRODUCTOS = [
+  {
+    nombre: "camiseta",
+    precio: 35
+  },
+  {
+    nombre: "camisa",
+    precio: 30
+  },
+  {
+    nombre: "pantalon",
+    precio: 40
+  },
+  {
+    nombre: "pantaloneta",
+    precio: 20
+  },
+  {
+    nombre: "medias",
+    precio: 15
+  }
+];
 
-    function actualizarTotal() {
-      const totalElemento = document.getElementById("total");
-      totalElemento.textContent = total;
-    }
+let total = 0;
+const contadorPrendas = {
+  camiseta: 0,
+  camisa: 0,
+  pantalon: 0,
+  pantaloneta: 0,
+  medias: 0
+};
+
+function agregarProducto(nombrePrenda) {
+  const producto = PRODUCTOS.find(producto => producto.nombre === nombrePrenda);
+  total += producto.precio;
+  actualizarTotal();
+  contadorPrendas[nombrePrenda]++;
+  actualizarContador(nombrePrenda);
+}
+
+function actualizarTotal() {
+  const totalElemento = document.getElementById("total");
+  totalElemento.textContent = total;
+}
+
+function actualizarContador(nombrePrenda) {
+  const contadorElemento = document.getElementById("contador-" + nombrePrenda);
+  contadorElemento.textContent = contadorPrendas[nombrePrenda];
+}
+
+function reiniciarCompra() {
+  total = 0;
+  for (const prenda in contadorPrendas) {
+    contadorPrendas[prenda] = 0;
+    actualizarContador(prenda);
+  }
+  actualizarTotal();
+}
+
+const productos = document.querySelectorAll("#productos li");
+productos.forEach(producto => {
+  producto.addEventListener("click", () => {
+    const nombrePrenda = producto.id;
+    agregarProducto(nombrePrenda);
+  });
+});
+
+
+
+const finalizarCompraButton = document.getElementById("finalizar-compra");
+finalizarCompraButton.addEventListener("click", () => {
+  console.log("Total de la compra: $" + total);
+  console.log("Número de prendas compradas:");
+  for (const prenda in contadorPrendas) {
+    console.log(prenda + ": " + contadorPrendas[prenda]);
+  }
+  reiniciarCompra();
+});
+
+const buscarPrendaButton = document.getElementById("buscar-prenda");
+buscarPrendaButton.addEventListener("click", () => {
+  const inputPrenda = document.getElementById("input-prenda");
+  const nombrePrenda = inputPrenda.value.toLowerCase(); 
+  const encontrada = PRODUCTOS.some(producto => producto.nombre === nombrePrenda);
+  console.log("Prenda disponible: " + encontrada);
+});
+
     
-    function terminarCompra() {
-      alert("¡Compra realizada! Total: $" + total);
-      total = 0;
-      actualizarTotal();
-    }
